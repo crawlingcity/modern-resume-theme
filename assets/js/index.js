4ac082dc21a190477c26651f62a7158e2d572334
Joao Cunha <joao.cunha@webcomum.com>
2020-06-07 23:39:39 +0100

    change dark mode colors

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,13 +4,3 @@ if (
 ) {
   document.body.classList.add("dark");
 }
-
-function toggleDark() {
-  document.body.classList.toggle("dark");
-  let darkToggle = document.getElementsByClassName("dark-toggle-i");
-  if (darkToggle.classList.contains("fa-sun")) {
-    darkToggle.classList.remove("fa-sun");
-  } else {
-    darkToggle.classList.add("fa-moon");
-  }
-};
